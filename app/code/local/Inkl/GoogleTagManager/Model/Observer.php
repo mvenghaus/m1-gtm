eff606e1a87dbb89955a8387bbd3d89c9d2bd309
Marcus <info@inklammern.de>
2017-07-21 10:46:34 +0200

    only generate dataLayer data when enabled

--- app/code/local/Inkl/GoogleTagManager/Model/Observer.php
+++ app/code/local/Inkl/GoogleTagManager/Model/Observer.php
@@ -6,6 +6,26 @@ use Inkl\GoogleTagManager\Schema\Id;
 class Inkl_GoogleTagManager_Model_Observer
 {
 
+	public function controller_front_send_response_before(Varien_Event_Observer $observer)
+	{
+		if (!Mage::helper('inkl_googletagmanager/config_general')->isEnabled())
+		{
+			return;
+		}
+
+		$response = $observer->getEvent()->getFront()->getResponse();
+
+		$googleTagManager = GoogleTagManager::getInstance();
+
+		Mage::dispatchEvent('googletagmanager_render_tag_before', ['google_tag_manager' => $googleTagManager]);
+
+		$response->setBody(str_replace(
+			Mage::helper('inkl_googletagmanager/config_general')->getTagPlaceholder(),
+			$googleTagManager->renderTag(new Id(Mage::helper('inkl_googletagmanager/config_general')->getId())),
+			$response->getBody()
+		));
+	}
+
 	public function googletagmanager_render_tag_before(Varien_Event_Observer $observer)
 	{
 		/** @var GoogleTagManager $googleTagManager */
@@ -63,22 +83,6 @@ class Inkl_GoogleTagManager_Model_Observer
 		{
 			Mage::getSingleton('inkl_googletagmanager/dataLayer_ecommerce_purchase')->handle($googleTagManager);
 		}
-
-	}
-
-	public function controller_front_send_response_before(Varien_Event_Observer $observer)
-	{
-		$response = $observer->getEvent()->getFront()->getResponse();
-
-		$googleTagManager = GoogleTagManager::getInstance();
-
-		Mage::dispatchEvent('googletagmanager_render_tag_before', ['google_tag_manager' => $googleTagManager]);
-
-		$response->setBody(str_replace(
-			Mage::helper('inkl_googletagmanager/config_general')->getTagPlaceholder(),
-			$googleTagManager->renderTag(new Id(Mage::helper('inkl_googletagmanager/config_general')->getId())),
-			$response->getBody()
-		));
 	}
 
 }
